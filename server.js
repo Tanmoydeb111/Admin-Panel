ce59a1321477e1c081bc002ba5e3413c66a724e4
Tanmoy Deb <tanmoydeb2002@gmail.com>
2024-09-08 22:19:16 +0530

    update server

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require('express');
 require('dotenv').config();
-const sequelize = require('./config/database'); // Sequelize instance
-
+const sequelize = require('./config/database');
+const { errors } = require('celebrate');
 const userRoutes = require('./routes/register');
 const roleRoutes = require('./routes/role');
 const authRoutes = require('./routes/authRoutes');
 const getAllUser = require('./routes/getAllUsers');
 const assignRole = require('./routes/assignRole');
+
+const createProject = require('./routes/createProject');
+const allAudit = require('./routes/auditacc');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.use('/', userRoutes);
-app.use('/', roleRoutes);
+app.use(
+  '/',
+  userRoutes,
+  roleRoutes,
+  getAllUser,
+  assignRole,
+  createProject,
+  allAudit
+);
+
 app.use('/auth', authRoutes);
-app.use('/', getAllUser);
-app.use('/', assignRole);
-// Sync Sequelize models with database
+
+app.use(errors());
+
+// Fallback error handling middleware for other errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
 sequelize
   .sync()
   .then(() => {
